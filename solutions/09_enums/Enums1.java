2ca7dd5f2673e413fec0df0015e8c2391a7e0bf4
arkid <kaleciarkid@gmail.com>
2026-07-04 15:38:00 +0200

    Add enums chapter

---
+++ solutions/09_enums/Enums1.java
@@ -0,0 +1,40 @@
+// Enums1.java
+//
+// An enum is a type with a fixed list of values:
+//
+//     enum Color { RED, GREEN, BLUE }
+//     Color c = Color.RED;
+//
+// Use it where Python code would pass around strings like "red" and hope
+// nobody misspells them. Enums work great in a switch:
+//
+//     switch (c) {
+//         case RED -> ...
+//     }
+//
+// Complete the Season enum and the `next` method (WINTER wraps around
+// to SPRING).
+
+import javalings.Check;
+
+public class Enums1 {
+    public static void main(String[] args) {
+        Check.equals(Season.SUMMER, next(Season.SPRING), "after spring comes summer");
+        Check.equals(Season.SPRING, next(Season.WINTER), "after winter comes spring");
+        Check.equals(4, Season.values().length, "there are four seasons");
+        Check.equals("AUTUMN", Season.AUTUMN.name(), "name() gives the text");
+    }
+
+    static Season next(Season s) {
+        return switch (s) {
+            case SPRING -> Season.SUMMER;
+            case SUMMER -> Season.AUTUMN;
+            case AUTUMN -> Season.WINTER;
+            case WINTER -> Season.SPRING;
+        };
+    }
+}
+
+enum Season {
+    SPRING, SUMMER, AUTUMN, WINTER
+}
---
+++ solutions/09_enums/Enums2.java
@@ -0,0 +1,41 @@
+// Enums2.java
+//
+// Enum values can carry data and have methods, because an enum is a class.
+//
+//     enum Planet {
+//         EARTH(5.97), MARS(0.642);
+//
+//         final double mass;
+//
+//         Planet(double mass) { this.mass = mass; }
+//     }
+//
+// Give each Coin a value in cents and implement `total`.
+
+import javalings.Check;
+
+public class Enums2 {
+    public static void main(String[] args) {
+        Check.equals(25, Coin.QUARTER.cents, "a quarter is 25 cents");
+        Check.equals(41, total(new Coin[]{Coin.QUARTER, Coin.DIME, Coin.NICKEL, Coin.PENNY}), "25 + 10 + 5 + 1");
+        Check.equals(0, total(new Coin[0]), "no coins");
+    }
+
+    static int total(Coin[] coins) {
+        int sum = 0;
+        for (Coin c : coins) {
+            sum += c.cents;
+        }
+        return sum;
+    }
+}
+
+enum Coin {
+    PENNY(1), NICKEL(5), DIME(10), QUARTER(25);
+
+    final int cents;
+
+    Coin(int cents) {
+        this.cents = cents;
+    }
+}
